a75313be2ad128664d917df7bca6df56921dd241
Shin <struct.developer@gmail.com>
2022-12-19 11:23:37 +0900

    Create Project

---
+++ WANNACD/WANNACD/WANNACDApp.swift
@@ -0,0 +1,17 @@
+//
+//  WANNACDApp.swift
+//  WANNACD
+//
+//  Created by Shin Jae Ung on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct WANNACDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
